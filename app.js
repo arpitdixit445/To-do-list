0259d62b2366751b978fd326b923d22f6b251a5e
Arpit <arpitdixit445@gmail.com>
2021-01-19 12:45:27 +0530

    add EJS, setup basic funtionality

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
+let tasks = ["Gajar launga", "Halua banaunga", "Halua khaunga"];
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html");
+    let d = new Date();
+    let options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    }
+
+    let day = d.toLocaleDateString("en-US", options);
+
+    res.render("list",{kindOfDay : day, tasks : tasks});
+});
+
+app.post("/", function(req, res){
+    const task = req.body.task;
+    if(task.length != 0){
+        tasks.push(task);
+    }
+    res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function(){
